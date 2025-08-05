ce4965ba104347d4ed5cacf5a77bcfbfa51ebf1e
BaherKh1 <thebestg890@gmail.com>
2025-08-05 22:07:01 +0200

    Document approval Workflow

---
+++ src/main/java/com/example/document_approval/DocumentApprovalWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.document_approval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentApprovalWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentApprovalWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/document_approval/DocumentApprovalWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.document_approval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentApprovalWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
